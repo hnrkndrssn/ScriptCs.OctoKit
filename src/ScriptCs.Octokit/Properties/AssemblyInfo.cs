b6c2680aeaea0818d96c74677d7a451a59a97281
Henrik Andersson <henrik.a.andersson@outlook.com>
2013-11-14 05:00:06 +1000

    Update octokit.net package to latest

--- src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
+++ src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("69341200-0230-4734-9F20-CE145B17043E")]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-build14")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
+[assembly: AssemblyInformationalVersion("0.1.4")]
