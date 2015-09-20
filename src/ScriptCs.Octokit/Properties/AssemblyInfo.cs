4f488ca39fb8876f770300bd68eae4eae6da1853
Henrik Andersson <henrik.a.andersson@outlook.com>
2015-09-20 20:24:26 +1000

    Update ALL THE THINGS

--- src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
+++ src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("69341200-0230-4734-9F20-CE145B17043E")]
 
-[assembly: AssemblyVersion("0.5.1")]
-[assembly: AssemblyFileVersion("0.5.1")]
-[assembly: AssemblyInformationalVersion("0.5.1")]
+[assembly: AssemblyVersion("0.5.2")]
+[assembly: AssemblyFileVersion("0.5.2")]
+[assembly: AssemblyInformationalVersion("0.5.2")]
