4c73c2f9319a1e541bd3b1b4e5f79ca606689ccb
Henrik Andersson <henrik@henrikandersson.org>
2019-02-17 15:42:30 +1000

    Convert project to vs2017 format

--- src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
+++ src/ScriptCs.Octokit/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
-using System.Reflection;
+ï»¿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("ScriptCs.Octokit")]
-[assembly: AssemblyDescription("Octokit Script Pack for ScriptCs.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Henrik Andersson")]
-[assembly: AssemblyProduct("ScriptCs.Octokit")]
-[assembly: AssemblyCopyright("Copyright © Henrik Andersson")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("69341200-0230-4734-9F20-CE145B17043E")]
-
-[assembly: AssemblyVersion("0.5.8")]
-[assembly: AssemblyFileVersion("0.5.8")]
-[assembly: AssemblyInformationalVersion("0.5.8")]
